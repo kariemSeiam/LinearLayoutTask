040c6f20a67856fa4a906bec9b5525685c6e50d6
Kariem Seiam <57973837+kariemSeiam@users.noreply.github.com>
2023-03-10 00:14:29 +0200

    Add unversioned files and update app files for LinearLayoutTask project

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
